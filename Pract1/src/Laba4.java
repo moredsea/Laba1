23bd320bb3439350b0343c955c94fdbc1e3d18e2
moredsea <70804023+moredsea@users.noreply.github.com>
2020-09-26 15:38:14 +0400

    Update Laba4.java

--- Pract1/src/Laba4.java
+++ Pract1/src/Laba4.java
@@ -1,2 +1,91 @@
-public class Laba4 {
-}
+public class Laba4{}
+    abstract class Shape {
+        protected double area;
+        protected double perimeter;
+        public abstract void calcArea();
+        public abstract void calcPerimeter();
+        public abstract double getArea();
+        public abstract double getPerimeter();
+    }
+
+     class Circle extends Shape {
+        private double D, R;
+
+        public Circle(double r) {
+            this.R = r;
+            this.D = 2 * this.R;
+        }
+
+        public void calcArea() {
+            area = Math.PI * Math.pow(this.R, 2);
+            System.out.println("Area of circle = " + area);
+        }
+
+        public void calcPerimeter() {
+            perimeter = 2 * Math.PI * this.R;
+            System.out.println("Perimeter of circle =" + perimeter);
+        }
+
+        public double getArea() {
+            return area;
+        }
+
+        public double getPerimeter() {
+            return perimeter;
+        }
+    }
+
+    class Rectangle extends Shape {
+
+        private double A, B;
+
+        public Rectangle(double a, double b) {
+            this.A = a;
+            this.B = b;
+        }
+
+        public void calcArea() {
+            area = this.A * this.B;
+            System.out.println("Area of rectangle = " + area);
+        }
+
+        public void calcPerimeter() {
+            perimeter = 2 * (this.B + this.A);
+            System.out.println("Perimeter of rectangle = " + perimeter);
+        }
+
+        public double getArea() {
+            return area;
+        }
+
+        public double getPerimeter() {
+            return perimeter;
+        }
+    }
+     class Triangle extends Shape {
+        double A,B,C;
+        public Triangle(double a, double b,double c)
+        {
+            this.A=a;
+            this.B=b;
+            this.C=c;
+        }
+        public void calcArea() {
+            double PP=(this.A+this.B+this.C)/2;
+            area=Math.sqrt(PP*(PP-this.A)*(PP-this.B)*(PP-this.C));
+            System.out.println("Area of triangle = "+ area);
+        }
+
+        public void calcPerimeter() {
+            perimeter = this.A+this.B+this.C;
+            System.out.println("Perimeter of triangle = "+perimeter);
+        }
+
+        public double getArea() {
+            return area;
+        }
+
+        public double getPerimeter() {
+            return perimeter;
+        }
+    }
